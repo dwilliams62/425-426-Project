9446de1393d978f9bf263fafe2788101ad2a8ab8
obrague1 <133675406+obrague1@users.noreply.github.com>
2023-12-07 09:48:13 -0500

    Got rid of this file

--- machineLearning/testingClassification.py
+++ machineLearning/testingClassification.py
@@ -1,6 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC  # Import the Support Vector Machine classifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import MaxAbsScaler
 import pandas as pd
@@ -8,9 +8,19 @@ import csv
 import os
 import re
 import spacy
+import json
 
 csvName = "allData.csv"
 allData = pd.read_csv(csvName, encoding="utf-8")  # Reading the data from allData.csv
+
+jsonPath = "Articles.json"
+with open(jsonPath, "r", encoding="utf-8") as jsonFile:
+    testData = json.load(jsonFile)
+
+newTitles = [item["title"] for item in testData]
+newAbstracts = [item["abstract"] for item in testData]
+
+newCombinedFeatures = [f"{title} {abstract}" for title, abstract in zip(newTitles, newAbstracts)]
 
 nlp = spacy.load("en_core_web_sm") # Loading Spacy's English module
 
@@ -30,18 +40,20 @@ def customTokenizer(text):
 combinedFeatures = []
 for title, abstract, tag in zip(titles, abstracts, tags):
     if title and abstract and tag:
-        combinedFeatures.append(f"{title} {abstract} {tag}") # Combining titles, abstracts, and tags together
+        combinedFeatures.append(f"{title} {abstract} {tag}") # Combining features of articles
 
-scaler = MaxAbsScaler()
+scaler = MaxAbsScaler()        
 
 tfidf_vectorizer = TfidfVectorizer(tokenizer=customTokenizer, lowercase=True)
 vectors = tfidf_vectorizer.fit_transform(combinedFeatures) # Vectorizing and tokenizing the contents of the abstracts, titles, and tags
-vectorsScaled = scaler.fit_transform(vectors) # Normalizing the vectors
+vectorsScaled = scaler.fit_transform(vectors) # Normalizing data from articles
+newVectors = tfidf_vectorizer.transform(newCombinedFeatures)
+newVectorsScaled = scaler.fit_transform(newVectors) # Normalizing new data that is being tested
 
 svm = SVC(C=1.0, kernel='linear', random_state=42)  # Initializing SVM
+svm.fit(vectorsScaled, labels) 
 
-X_train, X_test, y_train, y_test = train_test_split(vectorsScaled, labels, test_size=0.5, random_state=42) # Splitting training and testing data
-
-svm.fit(X_train, y_train)  # Fitting the training data to the SVM
-y_pred = svm.predict(X_test)
-print(classification_report(y_test, y_pred))
+newPredictions = svm.predict(newVectorsScaled) # Making predictions using the new data
+for title, prediction in zip(newTitles, newPredictions):
+    print(f"Title: {title}\tPrediction: {prediction}")
+    print("") # Printing article titles along with predictions  
