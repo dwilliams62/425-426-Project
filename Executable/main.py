566a80ad93284a21d09c5b28452b4bd2bd6186d3
Dylan Williams <dylan.williams.mmhs@gmail.com>
2023-12-22 17:31:38 -0500

    added comments and the search term for web scraping

--- Executable/main.py
+++ Executable/main.py
@@ -2,10 +2,13 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import Scale
 from tkinter import ttk
+
+# import functions from files that we define
 from fileFunctions import process_rdf_file, download_as_rdf, upload_to_website
 from websiteScraping import scrape_pubmed, scrape_springer
 from machineLearning import add_category
 
+# global variables that can be used for any function, stores the data we're working with and which website we're scraping
 processed_data = None
 website_chosen = None
 
@@ -28,30 +31,34 @@ def create_startup_buttons(root):
 
 
 # When the user chooses to upload their own RDF, this is where the data will be processed,
-# And then the user will be sent to the classifier
+# And then the user will be sent to the categorizer
 def upload_own_rdf():
-    global processed_data
+    global processed_data #reference the global variable to be able to update it
 
-    file_path = filedialog.askopenfilename(filetypes=[("RDF Files", "*.rdf")])
+    #prompts the user to pick a file on their computer to upload that ends in .rdf
+    file_path = filedialog.askopenfilename(filetypes=[("RDF Files", "*.rdf")]) 
 
+    #if the user picks an rdf file, processes it and then goes to the categorizer. otherwise does nothing
     if file_path:
-        processed_data = process_rdf_file(file_path)  # Assuming file_path is known or obtained from user input
-
+        processed_data = process_rdf_file(file_path)
         categorize_data_initialize()
 
 
 
+# starts the web scraper section, asks the user to pick a website and how many pages of data to scrape
+# then scrapes the data into processed_data before sending the program to the categorizer
 def scrape_website_initialize():
-    global processed_data
+    global processed_data #reference the global variable to be able to update it
 
+    #clear out all the current tkinter objects on the page
     for widget in root.winfo_children():
         widget.pack_forget()
     
-    # Label - Select a website to scrape from
+    # Create label
     website_label = tk.Label(root, text="Select a website to scrape from")
     website_label.pack()
 
-    # Buttons for website selection (Pubmed and Springer)
+    # Create the buttons to pick what website
     website_frame = tk.Frame(root)
     website_frame.pack()
 
@@ -61,68 +68,99 @@ def scrape_website_initialize():
     springer_button = tk.Button(website_frame, text="Springer", command=set_website_springer)
     springer_button.pack(side=tk.LEFT, padx=5)
 
-    # Label - How many pages of articles would you like to scrape?
-    pages_label = tk.Label(root, text="How many pages of articles would you like to scrape?")
-    pages_label.pack()
+    #create a label
+    label = tk.Label(root, text="Enter term to search by")
+    label.pack(pady=10)
 
-    # Scale widget for selecting the number of pages
+    #create a bar the user can type in a search term to search by
+    entry = tk.Entry(root)
+    entry.pack()
+
+    # Create another label
+    pages_label = tk.Label(root, text="How many pages of articles would you like to scrape?")
+    pages_label.pack(pady=10)
+
+    # Adds a scale bar to select number of pages from 1 to 10
     pages_scale = Scale(root, from_=1, to=10, orient=tk.HORIZONTAL, length=200)
     pages_scale.pack()
 
-    # Button to initiate scraping
-    scrape_button = tk.Button(root, text="Scrape!", command=lambda: scrape_website_process(pages_scale))
+    # Create the final submit button to start scraping
+    scrape_button = tk.Button(root, text="Scrape!", command=lambda: scrape_website_process(pages_scale, entry.get()))
     scrape_button.pack(pady=10)
 
+#if the springer button is pressed, use springer
 def set_website_springer():
     global website_chosen
     website_chosen = 'springer'
 
+#if the pubmed button is pressed, use pubmed
 def set_website_pubmed():
     global website_chosen
     website_chosen = 'pubmed'
 
-def scrape_website_process(scale_widget):
+#actually scrapes the websites then sends it to the categorizers
+def scrape_website_process(scale_widget, scrape_term):
+    #if no website is chosen, button does nothing
     if website_chosen:
+        #grabs the value of the scale
         value = scale_widget.get()
 
+        #removes all widgets on the gui currently
         for widget in root.winfo_children():
             widget.pack_forget()
 
+        #add a label
+        pages_label = tk.Label(root, text="Scraping....")
+        pages_label.pack()
+
         # Create a progress bar
         progress = ttk.Progressbar(root, orient="horizontal", length=200, mode="determinate")
-        progress.pack(padx=20, pady=20)
+        progress.pack(padx=20, pady=5)
 
-        global processed_data
+        global processed_data #reference global variable to change it
 
+        #depending on the website chosen, call correct scraping function
         if website_chosen == 'pubmed':
-            processed_data = scrape_pubmed(progress, value)
+            processed_data = scrape_pubmed(progress, value, scrape_term)
         if website_chosen == 'springer':
-            processed_data = scrape_springer(progress, value)
+            processed_data = scrape_springer(progress, value, scrape_term)
         
+        #start the categorizing of the data
         categorize_data_initialize()
         
 
 
-
+#at this point data will have been uploaded somehow, and the program will then use the machine learning algorithm to
+#to classify each article given into the correct category
 def categorize_data_initialize():
-    # Update the label text to indicate processing is complete
+    #remove all widgets currently inside
     for widget in root.winfo_children():
         widget.pack_forget()
 
+    #add a label to show what's happening
+    pages_label = tk.Label(root, text="Categorizing....")
+    pages_label.pack()
+    
     # Create a progress bar
     progress = ttk.Progressbar(root, orient="horizontal", length=200, mode="determinate")
-    progress.pack(padx=20, pady=20)
+    progress.pack(padx=20, pady=5)
 
-    global processed_data
+    global processed_data #reference global variable to change it
 
+    #using the current data, add the correct category to each article in processed_data
     processed_data = add_category(progress, processed_data)
+
+    #send the user to the final screen
     use_data_initialize()
 
+
+
+#from here the user will decide to download the data as an rdf or upload it to the website
 def use_data_initialize():
     for widget in root.winfo_children():
         widget.pack_forget()
 
-    # Label - What would you like to do with the data?
+    # define label
     label = tk.Label(root, text="What would you like to do with the data?")
     label.pack(pady=10)
 
@@ -133,6 +171,8 @@ def use_data_initialize():
     # Button - Download as Zotero RDF file
     download_button = tk.Button(root, text="Download as Zotero RDF file", command=lambda: download_as_rdf(processed_data))
     download_button.pack(pady=5)
+
+
 
 # Create the main window
 root = tk.Tk()
--- Executable/websiteScraping.py
+++ Executable/websiteScraping.py
@@ -2,8 +2,8 @@ def scrape_website():
     # Add your own logic here for scraping a website
     print("Scrape from website")
 
-def scrape_pubmed(progress_bar, page_count):
+def scrape_pubmed(progress_bar, page_count, scrape_term):
     progress_bar['value'] = page_count
 
-def scrape_springer(progress_bar, page_count):
+def scrape_springer(progress_bar, page_count, scrape_term):
     progress_bar['value'] = page_count
