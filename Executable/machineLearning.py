8e1330e63a290d524fbb50705eaf2517e8f22920
obrague1 <133675406+obrague1@users.noreply.github.com>
2024-01-30 14:15:12 -0500

    Adding to machine learning function

--- Executable/machineLearning.py
+++ Executable/machineLearning.py
@@ -1,3 +1,50 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC  # Import the Support Vector Machine classifier
+import pandas as pd
+import csv
+import os
+import re
+import spacy
+
+nlp = spacy.load("en_core_web_sm") # Loading Spacy's English module
+
+def customTokenizer(text):
+    doc = nlp(text)
+    tokens = [token.lemma_ for token in doc if token.is_alpha and not token.is_stop] # Extracts lemmatized tokens
+    bigrams = [" ".join(tokens[i:i+2]) for i in range(len(tokens) - 1)] # Creating bigrams to give more context to algorithm
+    allGrams = tokens + bigrams # Joining bigrams and tokens
+
+    return allGrams
+
 def add_category(progress_bar, data):
     progress_bar['value'] = 87
+
+    csvName = "allData.csv"
+    allData = pd.read_csv(csvName, encoding="utf-8")  # Reading the data from allData.csv
+
+    abstracts = allData["Abstract"].tolist()  # Extracting abstracts from the CSV
+    titles = allData["Title"].tolist()  # Extracting titles from the CSV
+    tags = allData["Tags"].tolist() # Extracting tags from the CSV
+    labels = allData["Category"].tolist()  # Extracting labels from the CSV
+
+    combinedFeatures = []
+    for title, abstract, tag in zip(titles, abstracts, tags):
+        if title and abstract and tag:
+            combinedFeatures.append(f"{title} {abstract} {tag}") # Combining features of articles
+
+    tfidf_vectorizer = TfidfVectorizer(tokenizer=customTokenizer, lowercase=True)
+    vectors = tfidf_vectorizer.fit_transform(combinedFeatures) # Vectorizing and tokenizing the contents of the abstracts, titles, and tags   
+
+    svm = SVC(C=1.0, kernel='linear', random_state=42)  # Initializing SVM
+    svm.fit(vectors, labels) 
+
+    for dictionary in data:
+        articleAbstract = dictionary["abstract"]
+        articleTitle = dictionary["title"]
+        dataCombined = " ".join(articleAbstract, articleTitle) # Combining abstract and title to be analyzed
+        newVector = tfidf_vectorizer.transform(dataCombined)
+
+        prediction = svm.predict(newVector)
+        dictionary["ourTags"] = prediction
+
     return data
