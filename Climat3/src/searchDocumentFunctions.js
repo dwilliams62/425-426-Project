11690e776f062833fd70ac8f14b660362852c7d0
Dylan Williams <dylan.williams.mmhs@gmail.com>
2024-02-28 10:59:08 -0500

    bug fixes and author display changes

--- Climat3/src/searchDocumentFunctions.js
+++ Climat3/src/searchDocumentFunctions.js
@@ -22,12 +22,13 @@ const db = getFirestore(app);
 
 var querySnapshot;
 var pageNumber = 1;
+var authorAll;
 
 export async function startSearch() {
   //first start by setting up the query based on the preference of what the user selected to search by. current default is title
   var q;
   if (document.getElementById('sortBy').value == 'title') {
-    q = query(collection(db, "Documents"), orderBy('Title')); 
+    q = query(collection(db, "Documents"), orderBy('title')); 
   }
   else if (document.getElementById('sortBy').value == 'author') {
     q = query(collection(db, "Documents"), orderBy('Authors')); 
@@ -87,11 +88,29 @@ function showSearchResults(minCountDocs, maxCountDocs) {
 
 //this function is mainly here to test the searchBy <select> tag to see what the user selected
 function searchBy(doc) {
+  //restart the current author string
+  authorAll = "";
+  
+  //get all the keys of the current document and sort them
+  var keys = Object.keys(doc.data()).sort();
+
+  //for each key in the document, check if it is an author, and if so add it to the author list
+  keys.forEach(function(key) {
+      // Check if the variable starts with "author"
+      if (key.startsWith("author_")) {
+          if (doc.data()[key] !== undefined) {
+              // Append data to the string if it exists
+              authorAll += doc.data()[key];
+              authorAll += ", ";
+          }
+      }
+  });
+
   //if the user selected title (which is the current default), process the data
   if (document.getElementById('searchBy').value == 'title') {
     //if the document's title contains the string that the user has searched, add it to the outputDiv in card form, 
     //and return true that something was added
-    if (doc.data().Title.toLowerCase().includes(document.getElementById('searchText').value.toLowerCase())) {
+    if (doc.data().title.toLowerCase().includes(document.getElementById('searchText').value.toLowerCase())) {
       createCard(doc);
       return true;
     }
@@ -99,7 +118,7 @@ function searchBy(doc) {
 
   //same thing but if the user picked author it checks author
   if (document.getElementById('searchBy').value == 'author') {
-    if (doc.data().Authors.toLowerCase().includes(document.getElementById('searchText').value.toLowerCase())) {
+    if (authorAll.toLowerCase().includes(document.getElementById('searchText').value.toLowerCase())) {
       createCard(doc);
       return true;
     }
@@ -118,7 +137,7 @@ function createCard(doc) {
   //create the header of the card with the title and a checkbox
   const cardHeaderDiv = document.createElement("div");
   cardHeaderDiv.classList.add("card-header");
-  cardHeaderDiv.textContent = doc.data().Title;
+  cardHeaderDiv.textContent = doc.data().title;
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.addEventListener('change', UpdateSelectedArray);
@@ -127,15 +146,15 @@ function createCard(doc) {
   //create a body of the card holding the author
   const cardBodyDiv = document.createElement("div");
   cardBodyDiv.classList.add("card-body");
-  cardBodyDiv.textContent = doc.data().Authors;
+  cardBodyDiv.textContent = authorAll;
 
   //create another body to hold the <a> tag that has the link provided for that document
   const cardBodyDiv2 = document.createElement("div");
   cardBodyDiv2.classList.add("card-body");
   const button = document.createElement("a");
-  button.href = doc.data().URL;
+  button.href = doc.data().url;
   button.classList.add("btn", "btn-primary");
-  button.textContent = doc.data().URL;
+  button.textContent = doc.data().url;
   cardBodyDiv2.appendChild(button);
 
   //append all the headers and bodys to the card, append the card to the ouptut div, then add a <p> tag for a little space between cards
